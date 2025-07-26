0d2d9eda5c21c1dcb15cfdca4b689c8404bd1efe
abhiraj2kp <abhiraj2kp@gmail.com>
2025-07-26 09:25:04 +0530

    added images into asset folder for usage

---
+++ src/ui/assets/image/index.ts
@@ -0,0 +1,18 @@
+const Images = {
+  labIcon: require('./labIcon.png'),
+  taskIcon: require('./taskIcon.png'),
+  playIcon: require('./playIcon.png'),
+  complete: require('./complete.png'),
+  demoIcon: require('./demoIcon.png'),
+  videoIcon: require('./videoIcon.png'),
+  community: require('./community.png'),
+  splashLogo: require('./splashLogo.png'),
+  lessonIcon: require('./lessonIcon.png'),
+  moduleIcon: require('./moduleIcon.png'),
+  discordIcon: require('./discordIcon.png'),
+  langugageIcon: require('./langugageIcon.png'),
+  videoDuration: require('./videoDuration.png'),
+  certificationIcon: require('./certificationIcon.png'),
+};
+
+export default Images;
